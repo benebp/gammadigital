e2592c3ffff91fe6803dad3680275cec6d029129
benebp <benebp@github.com>
2020-11-03 12:20:47 +0100

    connection to database

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 'use strict';
 
+require('dotenv').config()
 const express = require('express');
 const app = express();
 app.use(express.json());
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const PORT = 3000;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@gammadigital.rxvwy.mongodb.net/gamma?retryWrites=true&w=majority`;
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server"); 
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('connected');
