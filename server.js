ab4594733c536e5ddb776e2127d83fef69e103fa
benebp <benebp@github.com>
2020-11-03 19:10:12 +0100

    get records endpoints

--- server.js
+++ server.js
@@ -5,15 +5,35 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 const PORT = 3000;
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@gammadigital.rxvwy.mongodb.net/gamma?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@gammadigital.rxvwy.mongodb.net/gamma_db?retryWrites=true&w=majority`;
 
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server"); 
-  client.close();
-});
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('gamma_db')
+    const collection = db.collection('letters')
+    app.get('/records', (req, res) => {
+      collection.find().toArray()
+      .then(result => {
+        res.send(result);
+      })
+      .catch(error => console.error(error))
+    })
+    app.get('/records/:id', (req, res) => {
+      const searchedRecordId = req.params.id;
+      collection.find({'id': searchedRecordId}).toArray()
+      .then(result => {
+        if (result.length === 0) {
+          res.send('No record found by given ID')
+        } else {
+          res.send(result[0]);
+        }
+      })
+      .catch(error => console.error(error))
+    })
+  })
+  .catch(error => console.error(error));
 
 app.get('/', (req, res) => {
   res.send('connected');
