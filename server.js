0465b4c246a622292768eaefa8c21e2ffdb82917
benebp <benebp@github.com>
2020-11-04 08:37:27 +0100

    delete record endpoint, post endpoint fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -10,46 +10,65 @@ const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@gam
 
 MongoClient.connect(url, { useUnifiedTopology: true })
   .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('gamma_db')
-    const collection = db.collection('letters')
+    console.log('Connected to Database');
+    const db = client.db('gamma_db');
+    const collection = db.collection('records');
     app.get('/records', (req, res) => {
       collection.find().toArray()
-      .then(result => {
-        res.send(result);
-      })
-      .catch(error => console.error(error))
-    })
+        .then(result => {
+          if (result.length === 0) {
+            res.send('No saved records');
+          } else {
+            res.send(result);
+          };
+        })
+        .catch(error => console.error(error));
+    });
     app.get('/records/:id', (req, res) => {
       collection.find({'id': req.params.id}).toArray()
-      .then(result => {
-        if (result.length === 0) {
-          res.send('No record found by given ID')
-        } else {
-          res.send(result[0]);
-        }
-      })
-      .catch(error => console.error(error))
-    })
+        .then(result => {
+          if (result.length === 0) {
+            res.send('No record found by given ID');
+          } else {
+            res.send(result[0]);
+          };
+        })
+        .catch(error => console.error(error));
+    });
     app.post('/records', (req, res) => {
-      collection.find({'id': req.body.id}).toArray()
       if (!req.body.id) {
         res.send('No ID given');
-      } else if (collection.find({'id': req.body.id}).toArray().length !== 0) {
-        res.send('Given ID already exists');
       } else {
-        collection.insertOne(req.body)
-        .then(result => {
-          res.send(result);
-        })
-        .catch(error => console.error(error))
+        collection.find({'id': req.body.id}).toArray()
+          .then(result => {
+            if (result.length !== 0) {
+              res.send('Given ID already exists');
+            } else {
+              collection.insertOne(req.body);
+              res.send('Created');
+            };
+          })
+          .catch(error => console.error(error));
       }
-    })
+    });
+    app.delete('/records/:id', (req, res) => {
+      collection.findOneAndDelete({'id': req.params.id})
+        .then(result => {
+          if (!result.value) {
+            res.send('No record found by given ID');
+          } else {
+            res.send(result.value);
+          };
+        })
+        .catch(error => console.error(error));
+    });
   })
   .catch(error => console.error(error));
 
 app.get('/', (req, res) => {
-  res.send('connected');
+  res.redirect('/records');
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
+});
